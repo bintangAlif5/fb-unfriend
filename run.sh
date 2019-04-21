0b3d89cc98b4bdf8dd6802eb5e3a916f08f6bc99
bintangAlif5 <49284528+bintangAlif5@users.noreply.github.com>
2019-04-22 00:53:15 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -17,12 +17,13 @@ RR="\033[31;1m"; # Red light
 CC="\033[36;1m"; # Cyan light
 Ung="\033[95m";
 
-echo  $CC"ini buat running ea bastard \n $RRjangan di recode ea babyk."
-echo $GG"1). Buat ngambil token"
-echo $YY"2). buat running ea ajg"
+echo  $CC"ini buat running ea bastard \n $RRjangan di recode ea babyk.";
+echo
+echo $GG"1). Buat ngambil token";
+echo $YY"2). buat running ea ajg";
 echo $wd"00). Keluar bastrad ga guna"
-echo $GL""
-read -p "input -> " star; 
+echo $CC"";
+read -p " input -> " star; 
 
 if [ $star = 1 ] || [ $star = 1 ]
 then
@@ -40,6 +41,6 @@ if [ $star = 00 ] || [ $star = 0 ]
 then 
 sleep 1
 echo $Ung""
-figlet Keluar Anjeng
-echo $GL"Thx ea pukimak"
+figlet Keluar
+echo $CC" Thx ea pukimak"
 fi
