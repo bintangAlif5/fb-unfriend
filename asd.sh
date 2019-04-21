53e548eddde045b65bb66bdb6f8c86b6fdd41c41
bintangAlif5 <49284528+bintangAlif5@users.noreply.github.com>
2019-04-21 22:18:49 +0700

    Add files via upload

---
+++ asd.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+YY="\033[33;1m"; # Yellow light
+GG="\033[32;1m"; # Green light
+RR="\033[31;1m"; # Red light
+W="\033[00m"; # White light
+U="\033[95m"; # Purple Light
+CL="\033[36;1m"; # Cyan Light
+GL="\033[96;1m"; # Blue Aqua
+XX="\033[93"; # green
+
+clear
+sleep 2
+echo $RR"▒▒▒▒▒▒▒▒▄▄▄▄▄▄▄▄▒▒▒▒▒▒"
+echo "▒▒█▒▒▒▄██████████▄▒▒▒▒"
+echo "▒█▐▒▒▒████████████▒▒▒▒"
+echo "▒▌▐▒▒██▄▀██████▀▄██▒▒▒" $U"Created : Star@ScA√$RR"
+echo "▐┼▐▒▒██▄▄▄▄██▄▄▄▄██▒▒▒" $CL"From:" $GL"Sunda Cyber Army$YY"
+echo $YY"▐┼▐▒▒██████████████▒▒▒"
+echo "▐▄▐████─▀▐▐▀█─█─▌▐██▄▒ "
+echo "▒▒█████──────────▐███▌       "
+echo "▒▒█▀▀██▄█─▄───▐─▄███▀▒"
+echo $GG"▒▒█▒▒███████▄██████▒▒▒"
+echo "▒▒▒▒▒██████████████▒▒▒"
+echo "▒▒▒▒▒█████████▐▌██▌▒▒▒"
+echo "▒▒▒▒▒▐▀▐▒▌▀█▀▒▐▒█▒▒▒▒▒"
+echo "▒▒▒▒▒▒▒▒▒▒▒▐▒▒▒▒▌▒▒▒▒▒$W"
+echo $W"Facebook Auto Unfriend";
+echo $GL"InActive Users$W";
+echo "_____________________________________";
+echo ""
